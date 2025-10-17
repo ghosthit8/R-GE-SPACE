c955572193f95e95c2c417d880a407853429b9f4
ghosthit8 <ghosthit8@protonmail.com>
2025-10-17 17:24:03 -0500

    Create core.v.js

---
+++ js/core.v.js
@@ -0,0 +1,5 @@
+// js/core.v.js
+// Single place to control cache-busting for core. Import this file everywhere.
+// Example:
+//   import { endBoot } from './core.v.js';
+export * from './core.js?v=34';
