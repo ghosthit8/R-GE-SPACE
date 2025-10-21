6c39e555ddb2fc0b692db3507a68bc3fa7256c79
ghosthit8 <ghosthit8@protonmail.com>
2025-10-21 14:38:37 -0500

    Create assets/js/matchup.config.js

---
+++ assets/js/matchup.config.js
@@ -0,0 +1,4 @@
+// assets/js/matchup.config.js
+export const SUPABASE_URL = "https://tuqvpcevrhciursxrgav.supabase.co";
+export const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cXZwY2V2cmhjaXVyc3hyZ2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MDA0NDQsImV4cCI6MjA3MjA3NjQ0NH0.JbIWJmioBNB_hN9nrLXX83u4OazV49UokvTjNB6xa_Y";
+export const EDGE_URL = `${SUPABASE_URL}/functions/v1/global-timer-v2`;
