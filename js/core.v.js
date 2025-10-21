29d18cbd7b81fbd88843a80054df356c55aaced0
ghosthit8 <ghosthit8@protonmail.com>
2025-10-21 01:06:35 -0500

    Delete js directory

--- js/core.v.js
+++
@@ -1,5 +0,0 @@
-// js/core.v.js
-// Single place to control cache-busting for core. Import this file everywhere.
-// Example:
-//   import { endBoot } from './core.v.js';
-export * from './core.js?v=39';
