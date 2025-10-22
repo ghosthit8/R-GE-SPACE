cdf1955618c7fc24cc0a9e243059160ddf99d010
ghosthit8 <ghosthit8@protonmail.com>
2025-10-21 19:07:58 -0500

    Update matchup.config.js

--- assets/js/matchup.config.js
+++ assets/js/matchup.config.js
@@ -1,4 +1,6 @@
-// assets/js/matchup.config.js
-export const SUPABASE_URL = "https://tuqvpcevrhciursxrgav.supabase.co";
-export const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cXZwY2V2cmhjaXVyc3hyZ2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MDA0NDQsImV4cCI6MjA3MjA3NjQ0NH0.JbIWJmioBNB_hN9nrLXX83u4OazV49UokvTjNB6xa_Y";
+// matchup.config.js
+export const SUPABASE_URL  = 'https://tuqvpcevrhciursxrgav.supabase.co';
+export const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1cXZwY2V2cmhjaXVyc3hyZ2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MDA0NDQsImV4cCI6MjA3MjA3NjQ0NH0.JbIWJmioBNB_hN9nrLXX83u4OazV49UokvTjNB6xa_Y';
+
+// Edge function URL for the global timer
 export const EDGE_URL = `${SUPABASE_URL}/functions/v1/global-timer-v2`;
